3caaac8dd39316bfd62a1975c3cf5dd68198adfc
Doc1997 <anhtranduc1997@gmail.com>
2021-04-06 23:50:49 +0700

    Refactoring Item's code

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,49 +2,55 @@ interface ItemProps {
     icon?: string;
     boldNumberText?: string;
     subText?: string;
-  }
-  
-  interface ItemCollectionProps {
+}
+
+interface ItemCollectionProps {
     items: ItemProps[];
-  }
-  
-  function ItemIconComponent(props: ItemProps) {
-    return <div className={`item-icon fa ${props.icon} fa-2x`}></div>;
-  }
-  
-  function ItemTextComponent(props: ItemProps) {
+}
+
+function ItemIconComponent(props: ItemProps) {
+    let {icon} = props;
+
+    return <div className={`item-icon fa ${icon} fa-2x`}></div>;
+}
+
+function ItemTextComponent(props: ItemProps) {
+    let {boldNumberText, subText} = props;
+
     return (
-      <div className="item-sub-container">
-        <div className="item-big-bold-text">{props.boldNumberText}</div>
-        <div className="item-text">{props.subText}</div>
-      </div>
+        <div className="item-sub-container">
+            <div className="item-big-bold-text">{boldNumberText}</div>
+            <div className="item-text">{subText}</div>
+        </div>
     );
-  }
-  
-  function ItemComponent(props: ItemProps) {
+}
+
+function ItemComponent(props: ItemProps) {
+    let { icon, boldNumberText, subText } = props;
+
     return (
-      <div className="item">
-        <ItemIconComponent icon={props.icon} />
-        <ItemTextComponent
-          boldNumberText={props.boldNumberText}
-          subText={props.subText}
-        />      
-      </div>
+        <div className="item">
+            <ItemIconComponent icon={icon} />
+            <ItemTextComponent
+                boldNumberText={boldNumberText}
+                subText={subText}
+            />
+        </div>
     );
-  }
-  
+}
+
 export const ItemContainer = (props: ItemCollectionProps) => {
     return (
-    <div className="item-container">
-      {props.items.map((item) => {
-        return (
-        <ItemComponent
-          icon={item.icon}
-          boldNumberText={item.boldNumberText}
-          subText={item.subText}
-        />
-        );
-      })}
-    </div>
+        <div className="item-container">
+            {props.items.map((item) => {
+                return (
+                    <ItemComponent
+                        icon={item.icon}
+                        boldNumberText={item.boldNumberText}
+                        subText={item.subText}
+                    />
+                );
+            })}
+        </div>
     );
-  }
+}
